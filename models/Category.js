8f6b6c2adae8ef23c9bda74d224229e86bd28bcf
Steffan Carter <xisled09@gmail.com>
2022-06-12 19:15:34 -0400

    added to models folder

--- models/Category.js
+++ models/Category.js
@@ -5,7 +5,6 @@ const sequelize = require('../config/connection.js');
 class Category extends Model {}
 
 Category.init({
-    // define columns
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
